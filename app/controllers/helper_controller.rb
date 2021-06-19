03399aeafac3017058b06d6dd164d8dd030b9bb5
Farhan Tariq <farhantariq12b@gmail.com>
2021-06-19 14:16:29 +0500

    main:: files added

---
+++ app/controllers/helper_controller.rb
@@ -0,0 +1,7 @@
+class HelperController < ApplicationController
+  def show_svg(path)
+    File.open("app/assets/images/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
+end
---
+++ app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      blue: "#1A3050",
+      'blue-hover': '#293F5E',
+      'blue-child': '#1B9AD6',
+      'light-blue': '#1579A912',
+      'light-blue-background': 'rgba(21, 121, 169, 0.07)',
+      'border': '#C4C4C4',
+      lightBlue: '#293F5E',
+      white: '#FFFFFF',
+      darkGrey: '#5C6B80'
+    },
+    backgroundImage: theme => ({
+      'steps-1': 'url("../../assets/images/steps-1.jpg")',
+      'steps-2': 'url("../../assets/images/steps-2.jpg")',
+      'steps-3': 'url("../../assets/images/steps-3.jpg")'
+    }),
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
