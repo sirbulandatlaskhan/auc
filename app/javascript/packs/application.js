25668fa619a2adb41c2e1a908f2af4d8c9ecdac3
Farhan Tariq <farhantariq12b@gmail.com>
2021-06-20 15:26:04 +0500

    layout/sections added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require owl.carousel
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -12,4 +13,29 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "stylesheets/application" // Add this line
+import "stylesheets/application"
+
+document.addEventListener("turbolinks:load", function() {
+  $(".owl-carousel").owlCarousel({
+    autoWidth: false,
+    stagePadding: 50,
+    navigation: true,
+    nav:true,
+    responsive: true,
+    responsive:{
+      0:{
+        items:1,
+        nav:true
+      },
+      600:{
+        items:2,
+        nav:true
+      },
+      1000:{
+        items:3,
+        nav:true,
+        loop:false
+      }
+  }
+  });
+ });
